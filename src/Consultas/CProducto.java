3657d441ea7682b4bfed743eb8e7e23e4a0533e0
Diseño <Diseo@Diseo-PC>
2017-03-07 14:34:43 -0300

    Modificando Producto

--- src/Consultas/CProducto.java
+++ src/Consultas/CProducto.java
@@ -10,13 +10,18 @@ import Datos.DEmpleado;
 import Datos.DProducto;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Richard
  */
 public class CProducto {
+    int totalRegistros = 0;
+    String sSQL;
      private Conexion mysql = new Conexion(); //Instanciando la clase conexion
     private Connection cn = mysql.conectar();
  
@@ -56,6 +61,48 @@ public class CProducto {
          
   
     }
-    
+     public DefaultTableModel mostrar(String buscar) {
+
+        DefaultTableModel modelo;
+
+        String[] titulos = {"COD", "Nombre", "Direccion","Telefono","Email","Cedula","Login","pass","Estado","Acceso"};
+
+        String[] registros = new String[10];
+        totalRegistros = 0;
+        modelo = new DefaultTableModel(null, titulos);
+
+        sSQL = "select p.cod_persona , p.nombre_persona , p.direccion,p.telefono , "
+                + "p.email,u.rut_usuario,u.login,u.password,u.estado ,u.acceso from "
+                + " persona p inner join usuario u on p.cod_persona = u.cod_usuario "
+                + " where nombre_persona like '%" + buscar + "%' order by cod_persona desc";
+
+        try {
+
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sSQL);
+
+            while (rs.next()) {
+
+                registros[0] = rs.getString("cod_persona");
+                registros[1] = rs.getString("nombre_persona");
+                registros[2] = rs.getString("direccion");
+                registros[3] = rs.getString("telefono");
+                registros[4] = rs.getString("email");
+                registros[5] = rs.getString("rut_usuario");
+                registros[6] = rs.getString("login");
+                registros[7] = rs.getString("password");
+                registros[8] = rs.getString("estado");
+                registros[9] = rs.getString("acceso");
+                totalRegistros = totalRegistros + 1;
+                modelo.addRow(registros);
+            }
+            return modelo;
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+
+    }
     
 }
